795e1c689438ad3c1cc26d2db634eebc6a8bc264
Alexey Pogorelov <alexpogo471@gmail.com>
2020-06-24 02:15:42 +0300

    add three variable for separation getting random values, and add for each for get random values

--- src/org/example/HomeTask2.java
+++ src/org/example/HomeTask2.java
@@ -11,6 +11,14 @@ public class HomeTask2 {
         for (int i = 0; i < 70; i++) {
             numbers.add((int) (Math.random() * 500));
         }
+
+        int low = 0;
+        int average = 0;
+        int big = 0;
+
+        for (Integer number : numbers) {
+            number += (int) (Math.random() * 500);
+        }
     }
 
 }

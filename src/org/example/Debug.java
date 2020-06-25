d66f580677ba345ad14a07dfc5f3c7721289fffa
Alexander Karpyuk <karjuk.sasha@gmail.com>
2020-06-25 14:28:54 +0400

    Создал метод main и добавил один метод

--- src/org/example/Debug.java
+++ src/org/example/Debug.java
@@ -4,5 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Debug {
+    public static void main(String[] args) {
 
+    }
+
+    private static int getRandom() {
+        return (int) (Math.random() * Integer.MAX_VALUE);
+    }
 }

a91ff57582fd81e54e661f0411a4032df2bfade6
Alexander Karpyuk <karjuk.sasha@gmail.com>
2020-06-25 14:30:13 +0400

    Добавил приветствие и проиницилизировал список

--- src/org/example/Debug.java
+++ src/org/example/Debug.java
@@ -5,7 +5,12 @@ import java.util.List;
 
 public class Debug {
     public static void main(String[] args) {
-
+        System.out.println("Hello teacher!");
+        System.err.println("Create ArrayList");
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            list.add(getRandom());
+        }
     }
 
     private static int getRandom() {

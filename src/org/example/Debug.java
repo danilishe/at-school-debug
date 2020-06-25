5ad1db77aa30f6164c8fe8a57eb49e9f32fb17fb
Sergey Igoshin <macinq@gmail.com>
2020-06-25 17:19:01 +0400

    Исправлено поле defaults на подходящее по смыслу poor. Исправлен вывод System.err на System.out. Добавлена фича для сокращения социального неравенства: бедность уничтожена, количество буржуев сокращено. Да здравствует социализм! :)

--- src/org/example/Debug.java
+++ src/org/example/Debug.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Debug {
+  private static int billionare = 0;
+  private static int poor = 0;
+  private static int regular = 0;
+
   public static void main(String[] args) {
     List<Integer> persons = new ArrayList<>();
 
@@ -11,28 +15,63 @@ public class Debug {
       persons.add(getRandom());
     }
 
-    int billionare = 0;
-    int defaults = 0;
-    int regular = 0;
+    counting(persons);
 
-    for (Integer person : persons) {
-      person += getRandom();
-      if (person > 1_000_000_000) {
-        billionare++;
-      } else if (person > 0) {
-        regular++;
-      } else {
-        defaults++;
+    System.out.println("poor = " + poor);
+    System.out.println("regular = " + regular);
+    System.out.println("billionare = " + billionare);
+    System.out.println("regular + billionare + poor = " + (regular + billionare + poor));
+    System.out.println();
+
+    for (int i = 0; i < 100; i++) {
+      int temp = persons.get(i);
+      if (temp >= 1_000_000_000) {
+        persons.remove(i);
+        persons.add(i, temp - 1_000_000_000);
+      } else if (temp < 0) {
+        persons.remove(i);
+        persons.add(i, temp + 1_000_000_000);
       }
     }
 
-    System.err.println("defaults = " + defaults);
-    System.err.println("regular = " + regular);
-    System.err.println("billionare = " + billionare);
-    System.err.println("regular + billionare + defaults = " + (regular + billionare + defaults));
+    Debug.billionare = 0;
+    Debug.regular = 0;
+    Debug.poor = 0;
+
+    recounting(persons);
+
+    System.out.println("poor = " + poor);
+    System.out.println("regular = " + regular);
+    System.out.println("billionare = " + billionare);
+    System.out.println("regular + billionare + poor = " + (regular + billionare + poor));
   }
 
   private static int getRandom() {
     return (int) (Math.random() * Integer.MAX_VALUE);
+  }
+
+  public static void counting(List<Integer> persons) {
+    for (Integer person : persons) {
+      person += getRandom();
+      if (person >= 1_000_000_000) {
+        Debug.billionare++;
+      } else if (person > 0) {
+        Debug.regular++;
+      } else {
+        Debug.poor++;
+      }
+    }
+  }
+
+  public static void recounting(List<Integer> persons) {
+    for (Integer person : persons) {
+      if (person >= 1_000_000_000) {
+        Debug.billionare++;
+      } else if (person > 0) {
+        Debug.regular++;
+      } else {
+        Debug.poor++;
+      }
+    }
   }
 }

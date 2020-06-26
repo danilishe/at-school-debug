2d5f2eb68df130364beeffdb02dc7f3e1ebf1f46
Dmitriy <ivanovdm812@gmail.com>
2020-06-26 11:10:27 +0400

    добавили м-в персон с их доходами. Представили функцию getRandom()

--- src/org/example/Debug.java
+++ src/org/example/Debug.java
@@ -8,5 +8,13 @@ public class Debug {
     public static void main(String[] args) {
         System.out.println("Hello world!!");
         System.err.println("Error..");
+        List<Integer> persons = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            persons.add(getRandom());
+        }
+    }
+
+    private static Integer getRandom() {
+        return (int) (Math.random() * Integer.MAX_VALUE);
     }
 }

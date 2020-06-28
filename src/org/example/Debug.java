695ad282e23db4367d530443142b2b31d2fc2dee
Dmitriy-PC <badikov_dima@mail.ru>
2020-06-28 17:16:31 +0300

    Start writing a simple calculator. Added a method for input numbers from the keyboard.

--- src/org/example/Debug.java
+++ src/org/example/Debug.java
@@ -1,7 +1,26 @@
 package org.example;
 
+
+import java.util.Scanner;
+
 public class Debug {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+
     }
+
+    public static int getInt() {
+        System.out.println("Введите число");
+        int number;
+        if (scanner.hasNextInt()) {
+            number = scanner.nextInt();
+        } else {
+            System.out.println("Допущена ошибка при вводе числа. Попробуйте ещё раз");
+            scanner.next();
+            number = getInt();
+        }
+        return number;
+    }
+
 }

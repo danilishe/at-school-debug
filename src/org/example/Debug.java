87c6c1461c8fdecdfbe5f63dc4badb28af99dd0c
in5tillMotion <waifusonmymind@gmail.com>
2020-06-29 17:14:59 +0400

    cycle_for_persons

--- src/org/example/Debug.java
+++ src/org/example/Debug.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Debug {
     public static void main(String[] args) {
         List<Integer> persons = new ArrayList<>();
-
+        for (int i = 0; i < 100; i++) {
+            persons.add(getRandom());
     }
 }

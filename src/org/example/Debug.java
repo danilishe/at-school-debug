37d4c331fc3a466c2e44df659147dff76627b22e
in5tillMotion <waifusonmymind@gmail.com>
2020-06-29 17:16:45 +0400

    counter_persons

--- src/org/example/Debug.java
+++ src/org/example/Debug.java
@@ -9,6 +9,17 @@ public class Debug {
         for (int i = 0; i < 100; i++) {
             persons.add(getRandom());
     }
+        int billionaire = 0;
+        int defaults = 0;
+        int regular = 0;
+        for (Integer person : persons) {
+            person += getRandom();
+            if (person > 1_000_000_000 || person < 0) {
+                billionaire++;
+            } else if (person > 0)
+                regular++;
+            else defaults++;
+        }
 }
 
     private static int getRandom() {

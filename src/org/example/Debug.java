21ed9765972757c68270061a0bf5a275c14310ca
pashshryaev <pashizh@yandex.ru>
2020-06-30 19:09:25 +0400

    Провели рефакторинг и немного подправили

--- src/org/example/Debug.java
+++ src/org/example/Debug.java
@@ -4,16 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Debug {
+
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
-            list.add((int)(Math.random() * Integer.MAX_VALUE));
+            list.add(getMet());
         }
         int billionare =0;
         int regular = 0;
         int defaults = 0;
         for(Integer persons : list) {
-            persons += (int)(Math.random() * Integer.MAX_VALUE);
+            persons += getMet();
             if(persons > 1_000_000_000) {
                 billionare++;
             }
@@ -28,5 +29,9 @@ public class Debug {
         System.out.println("regular = " + regular);
         System.out.println("defaults = " + defaults);
         System.out.println("defaults + billionare + regular = " + (defaults + billionare + regular));
+    }
+
+    private static int getMet() {
+        return (int) (Math.random() * Integer.MAX_VALUE);
     }
 }

fd69e1c9d8ac631d0a1fb43a66230aebcfa78208
pashshryaev <pashizh@yandex.ru>
2020-06-30 19:11:52 +0400

    Добавили условие persons == 0

--- src/org/example/Debug.java
+++ src/org/example/Debug.java
@@ -21,7 +21,7 @@ public class Debug {
             }
             else if(persons > 0) {
                 regular++;
-            }
+            } else defaults++;
         }
         System.out.println("billionare = " + billionare);
         System.out.println("regular = " + regular);
